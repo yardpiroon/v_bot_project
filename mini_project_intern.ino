4708e7c464a678ab2919136e9cc14af06ff01741
yardpiroon <84771102+yardpiroon@users.noreply.github.com>
2021-07-09 07:08:17 +0700

    Add esp 32

---
+++ mini_project_intern.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x29);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Orientation Sensor Test"); Serial.println("");
+  if (!bno.begin())
+  {
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1);
+  }
+
+  delay(1000);
+}
+
+void loop() {
+  
+  sensors_event_t linearAccelData, accelerometerData, gravityData;
+  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+
+  Serial.println(bno.getEvent.acceleration.x);
+
+  
+
+  uint8_t system, gyro, accel, mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+  Serial.println();
+  Serial.print("Calibration: Sys=");
+  Serial.print(system);
+  Serial.print(" Gyro=");
+  Serial.print(gyro);
+  Serial.print(" Accel=");
+  Serial.print(accel);
+  Serial.print(" Mag=");
+  Serial.println(mag);
+
+  Serial.println("--");
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+}
